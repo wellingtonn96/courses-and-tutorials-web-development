8a46aa3af5f178d2031d198a3f5443d7aac49c3e
Wellington Santos <weltossousa@gmail.com>
2020-07-20 14:34:51 -0300

    adicionado username validation e trantando erros com o try catch

--- course-nestjs-udemy/task-management/src/auth/user.repository.ts
+++ course-nestjs-udemy/task-management/src/auth/user.repository.ts
@@ -1,17 +1,34 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const exists = this.findOne({ username });
+
+    if (exists) {
+    }
+
     const user = new User();
 
     user.username = username;
     user.password = password;
 
-    await user.save();
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username alredy exists!');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
