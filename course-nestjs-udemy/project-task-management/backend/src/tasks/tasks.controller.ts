0ffa2b48d490d6b6498090422497069a1bb85477
Wellington Santos <weltossousa@gmail.com>
2020-07-23 09:12:01 -0300

    renomeando nome de diretorio apagando diretorios e adicionando variaveis de ambiente

---
+++ course-nestjs-udemy/project-task-management/backend/src/tasks/tasks.controller.ts
@@ -0,0 +1,79 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  Query,
+  UsePipes,
+  ValidationPipe,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation-pipes';
+import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '../auth/user.entity';
+import { GetUser } from '../auth/get-user.decorator';
+import { Logger } from '@nestjs/common';
+
+@Controller('tasks')
+@UseGuards(AuthGuard())
+export class TasksController {
+  private logger = new Logger('TasksController');
+  constructor(private tasksService: TasksService) {}
+  @Get()
+  getTasks(
+    @Query(ValidationPipe) filterDto: GetTasksFilterDto,
+    @GetUser() user: User,
+  ): Promise<Task[]> {
+    this.logger.verbose(
+      `User "${user.username}" retrieving all tasks. filters: ${JSON.stringify(
+        filterDto,
+      )}`,
+    );
+    return this.tasksService.getTasks(filterDto, user);
+  }
+  @Get('/:id')
+  getTaskBydId(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    return this.tasksService.getTaskById(id, user);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTasks(
+    @Body() createTaskDto: CreateTaskDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    this.logger.verbose(
+      `User "${user.username}" creating a new task. Data: ${JSON.stringify(
+        createTaskDto,
+      )}`,
+    );
+    return this.tasksService.createTask(createTaskDto, user);
+  }
+  @Delete('/:id')
+  deleteTask(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.tasksService.deleteTask(id, user);
+  }
+  @Patch('/:id/status')
+  updateStatusTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    return this.tasksService.updateStatusTask(id, status, user);
+  }
+}
