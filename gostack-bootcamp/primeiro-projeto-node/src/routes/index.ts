265070e49695a9d972958ad3d1e2cff41080f3f4
Wellington Santos <weltossousa@gmail.com>
2020-07-30 09:23:56 -0300

    criando rotas de listar e criar appointments

--- gostack-bootcamp/primeiro-projeto-node/src/routes/index.ts
+++ gostack-bootcamp/primeiro-projeto-node/src/routes/index.ts
@@ -1,16 +1,39 @@
-import { Router } from 'express';
+import { Router, response } from 'express';
+import { parseISO } from 'date-fns';
+import { uuid } from 'uuidv4';
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
+interface IAppointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
-  const user = {
-    name,
-    email,
-  };
+const appointments = <IAppointment[]>[];
 
-  return response.json(user);
+routes.get('/appointments', (request, response) => {
+  return response.json(appointments);
+});
+
+routes.post('/appointments', (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parseDate = parseISO(date);
+
+    const appointment: IAppointment = {
+      id: uuid(),
+      provider,
+      date: parseDate,
+    };
+
+    appointments.push(appointment);
+
+    return response.json(appointment);
+  } catch (error) {
+    return response.json(400).json({ error: error.message });
+  }
 });
 
 export default routes;
--- primeiro-projeto-node/prettier.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  traillingComa: 'alt',
-  arrowParens: 'avoid',
-};
