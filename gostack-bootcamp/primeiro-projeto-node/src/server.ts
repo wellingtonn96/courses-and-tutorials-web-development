e800156219e788344bead7ec755ded53c59a0b71
Wellington Santos <weltossousa@gmail.com>
2020-08-02 09:15:26 -0300

    criando conexao com o banco de dados, gerando migrations e criando a tabela de appointments

--- gostack-bootcamp/primeiro-projeto-node/src/server.ts
+++ gostack-bootcamp/primeiro-projeto-node/src/server.ts
@@ -2,13 +2,13 @@ import express from 'express';
 
 import routes from './routes';
 
+import './database';
+
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
-
-app.get('/', (request, response) => response.json({ message: 'Hello World!' }));
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
