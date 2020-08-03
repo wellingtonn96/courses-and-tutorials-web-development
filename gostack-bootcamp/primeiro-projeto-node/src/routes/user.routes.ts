10301c96593f1cdbfc9338e79af0278fa86ec854
Wellington Santos <weltossousa@gmail.com>
2020-08-03 20:10:14 -0300

    adcionando upload de arquivos

--- gostack-bootcamp/primeiro-projeto-node/src/routes/user.routes.ts
+++ gostack-bootcamp/primeiro-projeto-node/src/routes/user.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 import CreateUserService from '../services/CreateUserService';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
@@ -18,5 +24,15 @@ usersRouter.post('/', async (request, response) => {
 
   return response.send(user);
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  }
+);
 
 export default usersRouter;
