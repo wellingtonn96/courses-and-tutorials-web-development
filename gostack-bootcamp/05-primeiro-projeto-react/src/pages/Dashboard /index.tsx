486e00dd91b37e81f9f87d5b8e298a8923708dbc
Wellington Santos <weltossousa@gmail.com>
2020-08-15 10:41:23 -0300

    create the issues page

--- gostack-bootcamp/05-primeiro-projeto-react/src/pages/Dashboard /index.tsx
+++ gostack-bootcamp/05-primeiro-projeto-react/src/pages/Dashboard /index.tsx
@@ -1,7 +1,7 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
-
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories, Error } from './style';
@@ -18,7 +18,15 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories'
+    );
+    if (storageRepositories) {
+      return JSON.parse(storageRepositories);
+    }
+    return [];
+  });
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>
@@ -43,6 +51,13 @@ const Dashboard: React.FC = () => {
     }
   }
 
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories)
+    );
+  }, [repositories]);
+
   return (
     <>
       <img src={logoImg} alt="github Explorer" />
@@ -61,7 +76,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="/test">
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -72,7 +90,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- gostack-bootcamp/05-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ gostack-bootcamp/05-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,7 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
+
+import { Header, RepositoryInfo, Issues } from './style';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    const [repository, setRepository] = useState<Repository | null>(null);
+    const [issues, setIssues] = useState([]);
+    api.get(`/repos/${params.repository}`).then((response) => {
+      console.log(response.data);
+    });
+
+    api.get(`/repos/${params.repository}/issues `).then((response) => {
+      console.log(response.data);
+    });
+  }, [params.repository]);
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/repositories/rocketseat/unform">
+          <FiChevronLeft size={20} />
+          voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
+            alt="avatar img"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repositorio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="/dashboard">
+          <img src="" alt="" />
+          <div>
+            <strong>sdfsdfsdfsd</strong>
+            <p>sdfsdsdfsd</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
