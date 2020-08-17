9d526941f0d6f0916073212c77241d363b5f421a
Wellington Santos <weltossousa@gmail.com>
2020-08-17 20:55:18 -0300

    isolete components

--- gostack-bootcamp/projeto_goBarber/web/src/SignIn/index.tsx
+++ gostack-bootcamp/projeto_goBarber/web/src/SignIn/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import { Container, Content, Background } from './styled';
+
+import Input from '../components/Input';
+import Button from '../components/Button';
+
 import logoImg from '../assets/logo.svg';
 
 const SignIn: React.FC = () => (
@@ -12,10 +16,15 @@ const SignIn: React.FC = () => (
       <form action="">
         <h1>Faça seu Logon</h1>
 
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
         <a href="forgot_password">Esqueci minha senha</a>
       </form>
 
