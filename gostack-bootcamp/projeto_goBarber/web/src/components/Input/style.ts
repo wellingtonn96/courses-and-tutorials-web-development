8d7dfac0aa53258a73dd6ad6c1879d78f49e6768
Wellington Santos <weltossousa@gmail.com>
2020-08-17 23:55:24 -0300

    valite field and improve the aplication usability

--- gostack-bootcamp/projeto_goBarber/web/src/components/Input/style.ts
+++ gostack-bootcamp/projeto_goBarber/web/src/components/Input/style.ts
@@ -1,6 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
   border: 2px solid #232129;
@@ -10,6 +15,19 @@ export const Container = styled.div`
 
   display: flex;
   align-items: center;
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #ff9900;
+      border-color: #ff9900;
+    `};
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9900;
+    `};
 
   & + div {
     margin-top: 8px;
--- gostack-bootcamp/projeto_goBarber/web/src/pages/SignUp/index.tsx
+++ gostack-bootcamp/projeto_goBarber/web/src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import { Container, Content, Background } from './styled';
 
 import Input from '../../components/Input';
@@ -9,9 +10,24 @@ import Button from '../../components/Button';
 import logoImg from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      console.log(data);
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail valido'),
+        password: Yup.string().min(6, 'No minimo 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
