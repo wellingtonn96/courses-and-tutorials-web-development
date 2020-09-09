71819f2f48576558c52f51c966f6044658a28011
Wellington Santos <weltossousa@gmail.com>
2020-09-09 11:51:11 -0300

    adicionando projeto mobile

--- gostack-bootcamp/projeto_goBarber/mobile/src/components/Input/index.tsx
+++ gostack-bootcamp/projeto_goBarber/mobile/src/components/Input/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TextInputProps } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 import { Container, TextInput } from './style';
 
 interface InputProps extends TextInputProps {
@@ -10,6 +11,7 @@ interface InputProps extends TextInputProps {
 const Button: React.FC<InputProps> = ({ name, icon, ...rest }) => {
   return (
     <Container>
+      <Icon name={icon} size={20} color="#666360" />
       <TextInput
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
