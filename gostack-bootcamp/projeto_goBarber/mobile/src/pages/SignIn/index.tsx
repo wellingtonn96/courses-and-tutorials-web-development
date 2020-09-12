5094550c29e16928218122865cfa5f2df6baf9ba
Wellington Santos <weltossousa@gmail.com>
2020-09-12 12:00:13 -0300

    adding signup page

--- gostack-bootcamp/projeto_goBarber/mobile/src/pages/SignIn/index.tsx
+++ gostack-bootcamp/projeto_goBarber/mobile/src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import {
   Image,
@@ -8,6 +8,9 @@ import {
   View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+
 import {
   Container,
   Title,
@@ -23,6 +26,11 @@ import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
 
 const Signin: React.FC = () => {
+  const formRef = useRef(null);
+  const navigation = useNavigation();
+
+  const handleSignIn = useCallback(() => {}, []);
+
   return (
     <>
       <KeyboardAvoidingView
@@ -39,18 +47,18 @@ const Signin: React.FC = () => {
             <View>
               <Title>Faça seu logon</Title>
             </View>
-
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
-            <Button
-              onPress={() => {
-                console.log(`Deu`);
-              }}
-            >
-              Entrar
-            </Button>
-
-            <ForgoutPassword onPress={() => {}}>
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
+              <Button
+                onPress={() => {
+                  console.log(`Deu`);
+                }}
+              >
+                Entrar
+              </Button>
+            </Form>
+            <ForgoutPassword>
               <ForgoutPasswordText>Esqueci minha senha</ForgoutPasswordText>
             </ForgoutPassword>
           </Container>
@@ -58,7 +66,7 @@ const Signin: React.FC = () => {
       </KeyboardAvoidingView>
 
       <CreateAccountButton>
-        <CreateAccountButtonText>
+        <CreateAccountButtonText onPress={() => navigation.navigate('SignUp')}>
           <Icon name="log-in" size={20} color="#ff9000" />
           Criar uma conta
         </CreateAccountButtonText>
--- gostack-bootcamp/projeto_goBarber/mobile/src/pages/SignUp/index.tsx
+++ gostack-bootcamp/projeto_goBarber/mobile/src/pages/SignUp/index.tsx
@@ -1,7 +1,58 @@
 import React from 'react';
 
-import { Container } from './style';
+import {
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Container, Title, BackToSignIn, BackToSignInText } from './style';
 
-const SignUp: React.FC = () => <Container />;
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-export default SignUp;
+import logoImg from '../../assets/logo.png';
+
+const Signin: React.FC = () => {
+  const navigation = useNavigation();
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <ScrollView>
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log(`Deu`);
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+
+        <BackToSignIn>
+          <BackToSignInText onPress={() => navigation.navigate('SignIn')}>
+            <Icon name="log-in" size={20} color="#fff" />
+            Voltar para logon
+          </BackToSignInText>
+        </BackToSignIn>
+      </KeyboardAvoidingView>
+    </>
+  );
+};
+
+export default Signin;
